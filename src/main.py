e514dff4bb08364467780bd61961622e3f1ab2fc
Stefanos Kanellopoulos <skanelo.ai@gmail.com>
2023-04-12 23:20:46 +0200

    Modify dummy main. Call add_two_random_numbers().

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
-def dummy_function() -> None:
-    print("Hello world!")
+""" This module is main module of our project."""
+
+from utils import add_two_random_numbers
+
+if __name__ == "__main__":
+    result = add_two_random_numbers()
+    print(f"The result of adding two random numbers is {result}.")
