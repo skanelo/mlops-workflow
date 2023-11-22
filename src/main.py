637ed9c8471bd14d5302dbde95d640d554624032
Stefanos <skanelo.ai@gmail.com>
2023-11-22 20:10:44 +0100

    Add function for subtraction and tests.

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ def add_two_numbers(a: int, b: int) -> int:
     return a + b
 
 
-# def subtract_two_numbers(a: int, b: int) -> int:
-#     """Subtract two numbers."""
-#     return a - b
+def subtract_two_numbers(a: int, b: int) -> int:
+    """Subtract two numbers."""
+    return a - b
 
 
 if __name__ == "__main__":
     today = datetime.today()
     print(f"Today is {today}")
     print(add_two_numbers(1, 2))
-    # print(subtract_two_numbers(1, 2))
+    print(subtract_two_numbers(1, 2))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 """Module for testing the main module."""
 
-from src.main import add_two_numbers
+from src.main import add_two_numbers, subtract_two_numbers
 
 
 def test_add_two_numbers_positive() -> None:
@@ -51,49 +51,49 @@ def test_add_two_numbers_type() -> None:
     assert isinstance(result, int)
 
 
-# def test_subtract_two_numbers_positive() -> None:
-#     """Test subtracting two positive numbers."""
-#     result = subtract_two_numbers(8, 3)
-#     assert result == 5
+def test_subtract_two_numbers_positive() -> None:
+    """Test subtracting two positive numbers."""
+    result = subtract_two_numbers(8, 3)
+    assert result == 5
 
 
-# def test_subtract_two_numbers_negative() -> None:
-#     """Test subtracting from a negative number a positive number."""
-#     result = subtract_two_numbers(-2, 7)
-#     assert result == -9
+def test_subtract_two_numbers_negative() -> None:
+    """Test subtracting from a negative number a positive number."""
+    result = subtract_two_numbers(-2, 7)
+    assert result == -9
 
 
-# def test_subtract_two_numbers_zero() -> None:
-#     """Test from zero to a positive number."""
-#     result = subtract_two_numbers(0, 9)
-#     assert result == -9
+def test_subtract_two_numbers_zero() -> None:
+    """Test from zero to a positive number."""
+    result = subtract_two_numbers(0, 9)
+    assert result == -9
 
 
-# def test_subtract_two_numbers_negative_result() -> None:
-#     """Test subtracting one negative number from a positive."""
-#     result = subtract_two_numbers(10, -4)
-#     assert result == 14
+def test_subtract_two_numbers_negative_result() -> None:
+    """Test subtracting one negative number from a positive."""
+    result = subtract_two_numbers(10, -4)
+    assert result == 14
 
 
-# def test_subtract_two_numbers_both_negative() -> None:
-#     """Test subtracting two negative numbers."""
-#     result = subtract_two_numbers(-8, -3)
-#     assert result == -5
+def test_subtract_two_numbers_both_negative() -> None:
+    """Test subtracting two negative numbers."""
+    result = subtract_two_numbers(-8, -3)
+    assert result == -5
 
 
-# def test_subtract_two_numbers_type() -> None:
-#     """Test subtract_two_numbers results type."""
-#     result = subtract_two_numbers(8, 3)
-#     assert isinstance(result, int)
+def test_subtract_two_numbers_type() -> None:
+    """Test subtract_two_numbers results type."""
+    result = subtract_two_numbers(8, 3)
+    assert isinstance(result, int)
 
-#     result = subtract_two_numbers(-2, 7)
-#     assert isinstance(result, int)
+    result = subtract_two_numbers(-2, 7)
+    assert isinstance(result, int)
 
-#     result = subtract_two_numbers(0, 9)
-#     assert isinstance(result, int)
+    result = subtract_two_numbers(0, 9)
+    assert isinstance(result, int)
 
-#     result = subtract_two_numbers(10, -4)
-#     assert isinstance(result, int)
+    result = subtract_two_numbers(10, -4)
+    assert isinstance(result, int)
 
-#     result = subtract_two_numbers(-8, -3)
-#     assert isinstance(result, int)
+    result = subtract_two_numbers(-8, -3)
+    assert isinstance(result, int)
