0d01ad806fde55b4907523fbf4f2fafea5fa59e4
Stefanos <skanelo.ai@gmail.com>
2023-11-27 00:14:39 +0100

    Exclude from the coverage report the main lines of execution.

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def subtract_two_numbers(a: int, b: int) -> int:
 
 
 if __name__ == "__main__":
-    today = datetime.today()
-    print(f"Today is {today}")
-    print(add_two_numbers(1, 2))
-    print(subtract_two_numbers(1, 2))
+    today = datetime.today()  # pragma: no cover
+    print(f"Today is {today}")  # pragma: no cover
+    print(add_two_numbers(1, 2))  # pragma: no cover
+    print(subtract_two_numbers(1, 2))  # pragma: no cover
